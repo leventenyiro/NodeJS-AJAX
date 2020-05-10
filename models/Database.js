1f32b671b833582c30d441487e3a2b1b3f7bf0b6
leventenyiro <nyiro.levente@gmail.com>
2020-05-10 12:55:08 +0200

    hibák kiíratása

--- models/Database.js
+++ models/Database.js
@@ -10,49 +10,59 @@ class Database {
     }
 
     getAll(req, callback) {
-        this.conn.connect(() => {
+        this.conn.connect((err) => {
+            if (err) throw err
             var sql = "SELECT * FROM raktar"
             if (req.query.search != undefined) {
                 sql += ` WHERE nev LIKE "${req.query.search}%"`
             }
             sql += " ORDER BY id"
             this.conn.query(sql, (err, result) => {
+                if (err) throw err
                 return callback(result)
             })
         })
     }
 
     post(req, callback) {
-        this.conn.connect(() => {
+        this.conn.connect((err) => {
+            if (err) throw err
             var sql = `INSERT INTO raktar (nev, ar, keszleten) VALUES ("${req.body.nev}", ${req.body.ar}, ${req.body.keszleten})`
             this.conn.query(sql, (err) => {
+                if (err) throw err
                 return callback(err)
             })
         })
     }
 
     getOne(req, callback) {
-        this.conn.connect(() => {
+        this.conn.connect((err) => {
+            if (err) throw err
             var sql = `SELECT * FROM raktar WHERE id = ${req.params.id}`
             this.conn.query(sql, (err, result) => {
+                if (err) throw err
                 return callback(result)
             })
         })
     }
 
     put(req, callback) {
-        this.conn.connect(() => {
+        this.conn.connect((err) => {
+            if (err) throw err
             var sql = `UPDATE raktar SET nev = "${req.body.nev}", ar = ${req.body.ar}, keszleten = ${req.body.keszleten} WHERE id = ${req.params.id}`
             this.conn.query(sql, (err) => {
+                if (err) throw err
                 return callback(err)
             })
         })
     }
 
     delete(req, callback) {
-        this.conn.connect(() => {
+        this.conn.connect((err) => {
+            if (err) throw err
             var sql = `DELETE FROM raktar WHERE id = ${req.params.id}`
-            this.conn.query(sql, () => {
+            this.conn.query(sql, (err) => {
+                if (err) throw err
                 return callback()
             })
         })
