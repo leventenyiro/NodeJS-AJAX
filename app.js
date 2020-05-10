c76eb75841d72d8f825b0cd49cd169e81146f233
leventenyiro <nyiro.levente@gmail.com>
2020-05-10 10:01:31 +0200

    export out public, new updates

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ var app = express()
 var bodyParser = require("body-parser")
 var router = require("./router")
 var path = require("path")
+var cors = require("cors")
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use("/public", express.static(path.join(__dirname, "/public")))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use("/", router)
+
+app.use((req, res) => {
+    res.status(404)
+    res.send("<h1>404 Error!</h1>")
+})
 
 app.listen(8080, () => {
     console.log("Server is running...")
 })
-
-// https://stackabuse.com/building-a-rest-api-with-node-and-express/
