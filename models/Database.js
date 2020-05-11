07b01556ca55684ed50be750aa73c3a61d39917c
leventenyiro <nyiro.levente@gmail.com>
2020-05-11 08:45:37 +0200

    async javascript in Database

--- models/Database.js
+++ models/Database.js
@@ -1,11 +1,8 @@
 class Database {
     constructor() {
         this.mysql = require("mysql")
+        this.async = require("async")
         this.conn = this.mysql.createPool({
-            connectionLimit : 1000,
-            connectTimeout  : 60 * 60 * 1000,
-            acquireTimeout  : 60 * 60 * 1000,
-            timeout         : 60 * 60 * 1000,
             host: "localhost",
             user: "root",
             password: "",
@@ -14,67 +11,78 @@ class Database {
     }
 
     getAll(req, callback) {
-        this.conn.getConnection((err, connection) => {
-            if (err) throw err
+        this.async.forever(() => {
+            this.conn.getConnection((err, connection) => {
+                if (err) throw err
 
-            var sql = "SELECT * FROM raktar"
-            if (req.query.search != undefined) {
-                sql += ` WHERE nev LIKE "${req.query.search}%"`
-            }
-            sql += " ORDER BY id"
+                var sql = "SELECT * FROM raktar"
+                if (req.query.search != undefined) {
+                    sql += ` WHERE nev LIKE "${req.query.search}%"`
+                }
+                sql += " ORDER BY id"
 
-            connection.query(sql, (err, result) => {
-                if (err) throw err;
-                connection.release()
-                return callback(result)
+                connection.query(sql, (err, result) => {
+                    if (err) throw err;
+                    connection.release()
+                    return callback(result)
+                })
             })
+            
         })
     }
 
     post(req, callback) {
-        this.conn.getConnection((err, connection) => {
-            if (err) throw err
-            var sql = `INSERT INTO raktar (nev, ar, keszleten) VALUES ("${req.body.nev}", ${req.body.ar}, ${req.body.keszleten})`
-            connection.query(sql, (err, result) => {
+        this.async.forever(() => {
+            this.conn.getConnection((err, connection) => {
                 if (err) throw err
-                connection.release()
-                return callback(result)
+                var sql = `INSERT INTO raktar (nev, ar, keszleten) VALUES ("${req.body.nev}", ${req.body.ar}, ${req.body.keszleten})`
+                connection.query(sql, (err, result) => {
+                    if (err) throw err
+                    connection.release()
+                    return callback(result)
+                })
             })
         })
     }
 
     getOne(req, callback) {
-        this.conn.getConnection((err, connection) => {
-            if (err) throw err
-            var sql = `SELECT * FROM raktar WHERE id = ${req.params.id}`
-            connection.query(sql, (err, result) => {
+        this.async.forever(() => {
+            this.conn.getConnection((err, connection) => {
                 if (err) throw err
-                connection.release()
-                return callback(result)
+                var sql = `SELECT * FROM raktar WHERE id = ${req.params.id}`
+                connection.query(sql, (err, result) => {
+                    if (err) throw err
+                    connection.release()
+                    return callback(result)
+                })
             })
         })
     }
 
     put(req, callback) {
-        this.conn.getConnection((err, connection) => {
-            if (err) throw err
-            var sql = `UPDATE raktar SET nev = "${req.body.nev}", ar = ${req.body.ar}, keszleten = ${req.body.keszleten} WHERE id = ${req.params.id}`
-            connection.query(sql, (err, result) => {
+        this.async.forever(() => {
+            this.conn.getConnection((err, connection) => {
                 if (err) throw err
-                connection.release()
-                return callback(result)
+                var sql = `UPDATE raktar SET nev = "${req.body.nev}", ar = ${req.body.ar}, keszleten = ${req.body.keszleten} WHERE id = ${req.params.id}`
+                connection.query(sql, (err, result) => {
+                    if (err) throw err
+                    connection.release()
+                    return callback(result)
+                })
             })
         })
     }
 
     delete(req, callback) {
-        this.conn.getConnection((err, connection) => {
-            if (err) throw err
-            var sql = `DELETE FROM raktar WHERE id = ${req.params.id}`
-            connection.query(sql, (err, result) => {
+        this.async.forever(() => {
+            this.conn.getConnection((err, connection) => {
                 if (err) throw err
-                connection.release()
-                return callback(result)
+                var sql = `DELETE FROM raktar WHERE id = ${req.params.id}`
+                connection.query(sql, (err, result) => {
+                    if (err) throw err
+                    connection.release()
+                    return callback(result)
+                })
             })
         })
     }
