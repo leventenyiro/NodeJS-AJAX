c0ba9db0f422947b9b32804fc737c4e6a467373a
Levente Nyírő <nyiro.levente@gmail.com>
2020-06-06 10:49:06 +0200

    statusbar repair

--- views/mobile/lib/pages/insert.dart
+++ views/mobile/lib/pages/insert.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Insert extends StatefulWidget {
   @override
@@ -6,16 +7,49 @@ class Insert extends StatefulWidget {
 }
 
 class _InsertState extends State<Insert> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: formInsert(),
+    return AnnotatedRegion(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.white,
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+            child: Column(children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    "Insert",
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              formInsert(),
+            ],
+            ),
+
+          ),
+        ),
+      ),
     );
+    
+
+
   }
 }
 
 Widget formInsert() {
   return Form(
+
     child: Column(
       children: <Widget>[
         TextField(
