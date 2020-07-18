61c34c212237fc6091a22c02a7182e044d2678d4
Levente Nyírő <nyiro.levente@gmail.com>
2020-07-18 10:17:58 +0200

    change c# code

--- views/desktop/RestApiCS/Program.cs
+++ views/desktop/RestApiCS/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -28,6 +29,7 @@ namespace RestApiCS
         static void muvelet()
         {
             Console.WriteLine("\nTörlés - D");
+            Console.WriteLine("\nÚj elem - I");
             Console.Write("Parancs: ");
             string parancs = Console.ReadLine();
             if (parancs == "D")
@@ -36,7 +38,28 @@ namespace RestApiCS
                 string url = "http://192.168.0.2:8080/products/" + Console.ReadLine();
                 WebRequest request = WebRequest.Create(url);
                 request.Method = "DELETE";
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                request.GetResponse();
+                Console.Clear();
+                list();
+            }
+            else if (parancs == "I")
+            {
+                Console.Write("\nNév: ");
+                string nev = Console.ReadLine();
+                Console.Write("\nÁr: ");
+                string ar = Console.ReadLine();
+                string keszleten = "1";
+                string url = "http://192.168.0.2:8080/products";
+                var data = new Dictionary<string, string>
+                {
+                    { "nev", nev },
+                    { "ar", ar },
+                    { "keszleten", keszleten }
+                };
+                var json = new FormUrlEncodedContent(data);
+                var client = new HttpClient();
+                client.PostAsync(url, json);
                 Console.Clear();
                 list();
             }
