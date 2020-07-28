313fd8005f4e060d1a8c7fa78ad96a705558dc5d
Levente Nyírő <nyiro.levente@gmail.com>
2020-07-28 11:04:22 +0200

    Modify Mailsend.js

--- models/Mailsend.js
+++ models/Mailsend.js
@@ -1,28 +1,45 @@
 class Mailsend {
-    constructor(req, emailVerificationId) {
+    constructor(req) {
         var nodemailer = require("nodemailer")
-
-        var transporter = nodemailer.createTransport({
+        this.transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
                 user: "trophien.noreply@gmail.com",
                 pass: ""
             }
         })
+        this.url = "http://localhost/nodejs/email.html?id="
+        this.req = req;
+    }
 
-        var url = "http://localhost/nodejs/email.html?id="
-        
-        var mailOptions = {
+    verification(emailVerificationId) {
+        this.mailOptions = {
             from: "trophien.noreply@gmail.com",
-            to: req.body.email,
+            to: this.req.body.email,
             subject: "Email verification",
             html: `
                 <h1>Verificate your E-mail address</h1>
-                <a href="${url}${emailVerificationId}">Verification</a>
+                <a href="${this.url}${emailVerificationId}">Verification</a>
                 `
         }
+        this.send()
+    }
+    
+    forgotPassword(forgotPasswordId) {
+        this.mailOptions = {
+            from: "trophien.noreply@gmail.com",
+            to: this.req.body.email,
+            subject: "Email verification",
+            html: `
+                <h1>Verificate your E-mail address</h1>
+                <a href="${this.url}${forgotPasswordId}">Verification</a>
+                `
+        }
+        this.send()
+    }
 
-        transporter.sendMail(mailOptions, (err) => {
+    send() {
+        this.transporter.sendMail(this.mailOptions, (err) => {
             if (err) throw err
         })
     }
